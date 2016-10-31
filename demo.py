5d8c87d916b4df77b73e60424c242b80eee3cf4a
Phate <phate334@gmail.com>
2016-11-01 00:54:27 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
+import json
 from PTTparser import parse_board, parse_article
 
 meta, articles = parse_board('https://www.ptt.cc/bbs/Gossiping/index.html')
-print(meta, articles)
+print(json.dumps((meta, articles)))
 
 meta, push_msg = parse_article(articles[1]['link'])
-print(meta, push_msg)
+print(json.dumps((meta, push_msg)))
